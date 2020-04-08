e3ab738bd8e8b6f3f91e1e9ec3c020eb6de07ed6
test1cobl <44349111+test1cobl@users.noreply.github.com>
2020-04-07 20:32:05 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#B0C4DE`,
-        theme_color: `#B0C4DE`,
+        background_color: `#008080`,
+        theme_color: `#008080`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
